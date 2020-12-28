c9dbaf93dde177a10d711482489e193e90249b83
hurikake06 <hurikake.oracle@gmail.com>
2020-12-28 22:45:20 +0900

    pages _appなど追加

---
+++ src/components/index.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+import { NextComponentType, NextPageContext } from "next"
+import styled from "styled-components"
+
+type Props = {
+  className?: string
+}
+
+const Component : NextComponentType<NextPageContext, {}, Props> = props => (
+  <div className={props.className}>Welcome to next.js!</div>
+)
+
+const StyledComponent = styled(Component)`
+color: #f00;
+`
+
+export default StyledComponent
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,27 @@
-import React from 'react'
+import React from "react"
+import Head from "next/head"
+import Component from "../components/index";
+import { css } from "styled-components";
 
-export default () => (<div>Hello World</div>)
+type Props = {
+  title: string
+}
+
+class App extends React.Component<Props> {
+  static async getInitialProps(): Promise<Props> {
+    return { title: "Hello world" }
+  }
+
+  render() {
+    return (
+      <>
+        <Head>
+          <title>{this.props.title}</title>
+        </Head>
+        <Component/>
+      </>
+    )
+  }
+}
+
+export default App
