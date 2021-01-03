890f292c35783bc668f69e85550829ec33d001cc
honda.ryuto <honda.ryuto@a-tm.co.jp>
2021-01-03 17:17:48 +0900

    API追加

--- src/components/post/top/index.tsx
+++ src/components/post/top/index.tsx
@@ -10,8 +10,13 @@ type Props = {
 const Component : NextComponentType<NextPageContext, {}, Props> = props => (
   <>
     <h1>Learn</h1>
-    <Link href="/posts/1"><a>post 01</a></Link>
     <div className={props.className}>Welcome to next.js!</div>
+    <div>
+      <ul>
+        <li><Link href="/posts/1"><a>記事01</a></Link></li>
+        <li><Link href="/api/hello"><a>hello API</a></Link></li>
+      </ul>
+    </div>
   </>
 )
 
